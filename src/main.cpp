2ecf01ad09b1fef1137cedd68f553adb2b5db8f6
IgorTimofeev <methodman1@mail.ru>
2024-08-25 14:01:30 +0300

    backup

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,66 @@
 
 Aircraft aircraft;
 
+
+// IV => should be a real random value
+#define AES_IV      0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, \
+                    0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F
+
+char plaintext[16];
+char encrypted[16];
+
+uint8_t key[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
+uint8_t iv[16] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,  0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F};
+esp_aes_context ctx;
+
+
+void encodetest()
+{
+
+	uint8_t copyIv[16];
+	memcpy(copyIv, iv, sizeof(iv));
+
+	memset( plaintext, 0, sizeof( plaintext ) );
+	strcpy( plaintext, "Hello, ESP32!" );
+
+
+	esp_aes_crypt_cbc( &ctx, ESP_AES_ENCRYPT, sizeof(plaintext), copyIv, (uint8_t*)plaintext, (uint8_t*)encrypted );
+
+	//See encrypted payload, and wipe out plaintext.
+	memset( plaintext, 0, sizeof( plaintext ) );
+	int i;
+	for( i = 0; i < 16; i++ )
+	{
+		Serial.printf( "%02x[%c]%c", encrypted[i], (encrypted[i]>31)?encrypted[i]:' ', ((i&0xf)!=0xf)?' ':'\n' );
+	}
+	Serial.printf( "\n" );
+
+	//Use the ESP32 to decrypt the CBC block.
+	memcpy(copyIv, iv, sizeof(iv));
+	esp_aes_crypt_cbc( &ctx, ESP_AES_DECRYPT, sizeof(encrypted), copyIv, (uint8_t*)encrypted, (uint8_t*)plaintext );
+
+	//Verify output
+	for( i = 0; i < 16; i++ )
+	{
+		Serial.printf( "%02x[%c]%c", plaintext[i],  (plaintext[i]>31)?plaintext[i]:' ', ((i&0xf)!=0xf)?' ':'\n' );
+	}
+	Serial.printf( "\n" );
+}
+
 void setup() {
 	Serial.begin(115200);
 
-	aircraft.begin();
+	esp_aes_init( &ctx );
+	esp_aes_setkey( &ctx, key, 128 );
+
+//	aircraft.begin();
 }
 
 void loop() {
-	aircraft.tick();
+//	aircraft.tick();
+//
+//	delay(1000);
 
 	delay(1000);
+	encodetest();
 }
--- src/transceiver.cpp
+++ src/transceiver.cpp
@@ -2,31 +2,17 @@
 #include "aircraft.h"
 #include "packet.h"
 
-#define RF_FREQUENCY                                915000000 // Hz  center frequency
-#define TX_OUTPUT_POWER                             22        // dBm tx output power
-#define LORA_BANDWIDTH                              4         // bandwidth
-// 2: 31.25Khz
-// 3: 62.5Khz
-// 4: 125Khz
-// 5: 250KHZ
-// 6: 500Khz
-#define LORA_SPREADING_FACTOR                       7         // spreading factor [SF5..SF12]
-#define LORA_CODINGRATE                             1         // [1: 4/5,
-//  2: 4/6,
-//  3: 4/7,
-//  4: 4/8]
-
-#define LORA_PREAMBLE_LENGTH                        8         // Same for Tx and Rx
-#define LORA_PAYLOADLENGTH                          0         // 0: Variable length packet (explicit header)
-// 1..255  Fixed length packet (implicit header)
-
 void Transceiver::begin() {
-	_AESLib.gen_iv(_AESIV);
-	_AESLib.set_paddingmode(paddingMode::CMS);
+
 
 	Serial.println("[SX1262] Initializing");
 
-	auto state = _radio.begin(RF_FREQUENCY, TX_OUTPUT_POWER);
+	auto state = _radio.begin(
+		// Hz  center frequency
+		915000000,
+		// dBm tx output power
+		22
+	);
 
 	if (state == ERR_NONE) {
 
@@ -40,13 +26,30 @@ void Transceiver::begin() {
 	}
 
 	_radio.LoRaConfig(
-		LORA_SPREADING_FACTOR,
-		LORA_BANDWIDTH,
-		LORA_CODINGRATE,
-		LORA_PREAMBLE_LENGTH,
-		LORA_PAYLOADLENGTH,
-		true,               //crcOn
-		false);             //invertIrq
+		// spreading factor [SF5..SF12]
+		7,
+		// bandwidth
+		// 2: 31.25Khz
+		// 3: 62.5Khz
+		// 4: 125Khz
+		// 5: 250KHZ
+		// 6: 500Khz
+		4,
+		// [1: 4/5,
+		//  2: 4/6,
+		//  3: 4/7,
+		//  4: 4/8]
+		1,
+		// Same for Tx and Rx
+		8,
+		// 0: Variable length packet (explicit header)
+		// 1..255  Fixed length packet (implicit header)
+		0,
+		//crcOn
+		true,
+		//invertIrq
+		false
+	);
 }
 
 void Transceiver::tick(Aircraft &aircraft) {
@@ -57,88 +60,155 @@ void Transceiver::tick(Aircraft &aircraft) {
 	Packet<AHRSPacket> packet(
 		packetHeader,
 		PacketType::AHRS,
+//		AHRSPacket {
+//			.pitch = ahrs.getPitch(),
+//			.roll = ahrs.getRoll(),
+//			.yaw = ahrs.getYaw(),
+//
+//			.temperature = ahrs.getTemperature(),
+//			.pressure = ahrs.getPressure(),
+//
+//			.qnh = ahrs.getQnh(),
+//			.altitude = ahrs.getAltitude()
+//		}
 		AHRSPacket {
-			.pitch = ahrs.getPitch(),
-			.roll = ahrs.getRoll(),
-			.yaw = ahrs.getYaw(),
+			.pitch = 10,
+			.roll = 10,
+			.yaw = 10,
 
-			.temperature = ahrs.getTemperature(),
-			.pressure = ahrs.getPressure(),
+			.temperature = 10,
+			.pressure = 10,
 
-			.qnh = ahrs.getQnh(),
-			.altitude = ahrs.getAltitude()
+			.qnh = 10,
+			.altitude = 10
 		}
 	);
+
+	Serial.println("[AES] Initializing");
+
+	uint8_t _AESIV[16] = { 0x17, 0x5D, 0x25, 0x2A, 0xFD, 0x72, 0x1E, 0x01, 0x02, 0x60, 0x88, 0x92, 0x9A, 0x9B, 0x2A, 0xA9 };
+
+
+	uint8_t _AESBuffer[255];
+
+	esp_aes_context _AESContext = esp_aes_context();
+	esp_aes_init(&_AESContext);
 
 	Serial.println("[AES] Encrypting packet");
 
 	uint16_t packetLength = sizeof(packet);
-	uint16_t prefixLength = sizeof(packet.header);
+	uint16_t headerLength = sizeof(packet.header);
 	uint16_t typeLength = sizeof(packet.type);
 
 	// Copying header
-	mempcpy(&_AESBuffer, &packet, prefixLength);
+	mempcpy(&_AESBuffer, &packet, headerLength);
 
 	// Encrypting
-	uint8_t iv[sizeof(_AESIV)];
-	memcpy(iv, _AESIV, sizeof(_AESIV));
 
-	auto encryptedBodyLength = _AESLib.encrypt(
-		(uint8_t*) &packet + prefixLength,
-		packetLength - prefixLength,
-		(uint8_t*) &_AESBuffer + prefixLength,
-		_AESKey,
-		sizeof(_AESKey),
-		iv
+	auto nonEncryptedBodyLength = packetLength - headerLength;
+
+	Serial.print("[AES] nonEncryptedBodyLength: ");
+	Serial.println(nonEncryptedBodyLength);
+
+	Serial.println("[AES] Bytes: ");
+
+	auto nonEncryptedPtr = (uint8_t*) &packet + headerLength;
+
+	for (uint8_t* i = nonEncryptedPtr; i < nonEncryptedPtr + nonEncryptedBodyLength; i++) {
+		Serial.print(*i);
+		Serial.print(" ");
+	}
+
+	Serial.println();
+
+	uint8_t _AESKey[16] = { 0x02, 0xEB, 0x46, 0x45, 0x96, 0xB0, 0xD6, 0xB9, 0x7C, 0x34, 0xBE, 0x77, 0x75, 0xF2, 0xBE, 0x1B };
+	esp_aes_setkey(&_AESContext, _AESKey, 128);
+
+	esp_aes_crypt_cbc(
+		&_AESContext,
+		ESP_AES_ENCRYPT,
+		nonEncryptedBodyLength,
+		_AESIV,
+		(uint8_t*) &packet + headerLength,
+		(uint8_t*) &_AESBuffer + headerLength
 	);
 
+	auto encryptedBodyLength = nonEncryptedBodyLength + 16 - (nonEncryptedBodyLength % 16);
+
+	Serial.print("[AES] encryptedBodyLength: ");
+	Serial.println(encryptedBodyLength);
+
 	// Twemp, for sdimulation
+
 	uint8_t receiveBuffer[255];
-	uint16_t receivedPacketLength = prefixLength + encryptedBodyLength;
+	uint16_t receivedPacketLength = headerLength + encryptedBodyLength;
 	memcpy(&receiveBuffer, &_AESBuffer, receivedPacketLength);
 
+	Serial.print("[AES] receivedPacketLength: ");
+	Serial.println(receivedPacketLength);
+
+	auto packetPtr = (uint8_t*) &receiveBuffer;
+	Serial.println("[AES] Bytes: ");
+
+	for (uint8_t* i = packetPtr; i < packetPtr + receivedPacketLength; i++) {
+		Serial.print(*i);
+		Serial.print(" ");
+	}
+
+	Serial.println();
+
 	if (((uint32_t*) &receiveBuffer)[0] == packetHeader) {
-		Serial.println("Norm header");
+		Serial.println("[AES] Norm header");
 
 		Serial.println("[AES] Decrypting packet");
 
-		memcpy(iv, _AESIV, sizeof(_AESIV));
+		uint8_t pizda_AESIV[16] = { 0x17, 0x5D, 0x25, 0x2A, 0xFD, 0x72, 0x1E, 0x01, 0x02, 0x60, 0x88, 0x92, 0x9A, 0x9B, 0x2A, 0xA9 };
+		uint8_t pizda_AESKey[16] = { 0x02, 0xEB, 0x46, 0x45, 0x96, 0xB0, 0xD6, 0xB9, 0x7C, 0x34, 0xBE, 0x77, 0x75, 0xF2, 0xBE, 0x1B };
+		uint8_t pizda_AESBuffer[255];
 
-		_AESLib.decrypt(
-			(uint8_t*) &receiveBuffer + prefixLength,
-			receivedPacketLength - prefixLength,
-			_AESBuffer,
-			_AESKey,
-			sizeof(_AESKey),
-			iv
+		esp_aes_context pizda_AESContext = esp_aes_context();
+		esp_aes_init(&pizda_AESContext);
+
+		esp_aes_setkey(&pizda_AESContext, pizda_AESKey, 128);
+
+		esp_aes_crypt_cbc(
+			&pizda_AESContext,
+			ESP_AES_DECRYPT,
+			receivedPacketLength - headerLength,
+			pizda_AESIV,
+			(uint8_t*) &receiveBuffer + headerLength,
+			pizda_AESBuffer
 		);
 
-		auto decryptedPacketType = (PacketType) _AESBuffer[0];
-		Serial.print("Decrypted packet type: ");
+		auto decryptedPacketType = (PacketType) pizda_AESBuffer[0];
+		Serial.print("[AES] Decrypted packet type: ");
 		Serial.println(decryptedPacketType);
 
-		//	Serial.println("Bytes: ");
-		//
-		//	for (uint16_t i = 0; i < receivedPacketLength; i++) {
-		//		Serial.print(_AESBuffer[i]);
-		//		Serial.print(" ");
-		//	}
-		//
-		//	Serial.println();
+		Serial.println("[AES] Bytes: ");
+
+		for (uint16_t i = 0; i < receivedPacketLength; i++) {
+			Serial.print(_AESBuffer[i]);
+			Serial.print(" ");
+		}
+
+		Serial.println();
 
 		switch (decryptedPacketType) {
 			case PacketType::AHRS:
-				auto decryptedPacket = (AHRSPacket*) ((uint8_t*) &_AESBuffer + typeLength);
+				auto decryptedPacket = (AHRSPacket*) ((uint8_t*) &pizda_AESBuffer + typeLength);
 
 				decryptedPacket->print();
 
 				break;
 		}
+
+		esp_aes_free(&pizda_AESContext);
 	}
 	else {
 		Serial.println("Bad header");
 	}
 
+	esp_aes_free(&_AESContext);
 
 
 //	Serial.println("[SX1262] Sending packet");
